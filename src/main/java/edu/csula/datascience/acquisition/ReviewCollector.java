44d5e2ced6aa5c65fb7bbb4a73766e1c481eb454
SuperMaddie <mahdiye.jamali@gmail.com>
2016-06-12 22:01:44 -0700

    Revert "jest example added"
    This reverts commit 68120168293b72c603624615595770325fb0c4ad.

--- src/main/java/edu/csula/datascience/acquisition/ReviewCollector.java
+++ src/main/java/edu/csula/datascience/acquisition/ReviewCollector.java
@@ -31,29 +31,19 @@ public class ReviewCollector implements Collector<Review, Review> {
 	@Override
 	public Review mungee(Review data) {
 		boolean valid = data.getReviewerID() != null && data.getAsin() != null && data.getReviewText() != null;
-		return (valid && isRecentReview(data)) ? data : null;
+		return valid ? data : null;
 	}
 
-	public boolean isRecentReview(Review data) {
-		String rTime = data.getReviewTime();
-		if(rTime.isEmpty()) return false;
-		String[] splits = rTime.split(",");
-		int year = Integer.parseInt(splits[splits.length - 1].replaceAll("\\s+", ""));
-		return year > 2011;
-	}
-	
 	@Override
 	public void save(Review data) {
 		if (data == null)
 			return;
 		String id = data.getReviewerID().hashCode() + "" + data.getAsin().hashCode();
-		data.setDatetime(System.currentTimeMillis());
 		Document document = new Document().append("_id", id).append("reviewerID", data.getReviewerID()).append("asin", data.getAsin())
 				.append("reviewerName", data.getReviewerName()).append("helpful", data.getHelpful())
 				.append("reviewText", data.getReviewText()).append("overall", data.getOverall())
 				.append("summary", data.getSummary()).append("unixReviewTime", data.getUnixReviewTime())
-				.append("reviewTime", data.getReviewTime())
-				.append("datetime", data.getDatetime());
+				.append("reviewTime", data.getReviewTime());
 		try{
 			collection.insertOne(document);
 		}catch(Exception e){
